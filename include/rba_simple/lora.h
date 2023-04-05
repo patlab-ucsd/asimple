5c205dbd102cfb36c7e9b5d7f18c2ddc4e804009
Gabriel Marcano <gabemarcano@yahoo.com>
2023-04-05 13:23:26 -0700

    Add defines for lora registers

--- include/rba_simple/lora.h
+++ include/rba_simple/lora.h
@@ -10,6 +10,74 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+// Radio registers, from the Semtech SX1276 datasheet
+#define LORA_FIFO 0x00u
+#define LORA_OPMODE 0x01u
+#define LORA_FREQ_MSB 0x06u
+#define LORA_FREQ_MID 0x07u
+#define LORA_FREQ_LSB 0x08u
+#define LORA_PA_CONFIG 0x09u
+#define LORA_PA_RAMP 0x0Au
+#define LORA_PA_OCP 0x0Bu
+#define LORA_LNA 0x0Cu
+#define LORA_FIFO_ADDR 0x0Du
+#define LORA_FIFO_TX_BASE 0x0Eu
+#define LORA_FIFO_RX_BASE 0x0Fu
+#define LORA_FIFO_RX_CURRENT_ADDR 0x10u
+#define LORA_IRQ_FLAGS_MASK 0x11u
+#define LORA_IRQ_FLAGS 0x12u
+#define LORA_RX_BYTES 0x13u
+#define LORA_RX_HEADER_CNT_MSB 0x14u
+#define LORA_RX_HEADER_CNT_LSB 0x15u
+#define LORA_RX_PACKET_CNT_MSB 0x16u
+#define LORA_RX_PACKET_CNT_LSB 0x17u
+#define LORA_MODEM_STATUS 0x18u
+#define LORA_PACKET_SNR 0x19u
+#define LORA_PACKET_RSSI 0x1Au
+#define LORA_RSSI 0x1Bu
+#define LORA_HOP_CHANNEL 0x1Cu
+#define LORA_MODEM_CONFIG1 0x1Du
+#define LORA_MODEM_CONFIG2 0x1Eu
+#define LORA_SYMBOL_TIMEOUT 0x1Fu
+#define LORA_PREAMBLE_LEN_MSB 0x20u
+#define LORA_PREAMBLE_LEN_LSB 0x21u
+#define LORA_PAYLOAD_LEN 0x22u
+#define LORA_MAX_PAYLOAD_LEN 0x23u
+#define LORA_HOP_PERIOD 0x24u
+#define LORA_FIFO_RX_BYTE_ADDR 0x25u
+#define LORA_MODEM_CONFIG3 0x26u
+#define LORA_PPM_CORRECTION 0x27u
+#define LORA_FREQ_ERROR_MSB 0x28u
+#define LORA_FREQ_ERROR_MID 0x29u
+#define LORA_FREQ_ERROR_LSB 0x2Au
+#define LORA_RESERVED1 0x2Bu
+#define LORA_RSSI_WIDEBAND 0x2Cu
+#define LORA_RESERVED2 0x2Du
+#define LORA_RESERVED3 0x2Eu
+// See ERRATA 2.3 Receiver Spurious Reception of a LoRa Signal
+#define LORA_INTERMEDIATE_FREQ2 0x2Fu
+#define LORA_INTERMEDIATE_FREQ1 0x30u
+
+#define LORA_DETECT_OPTIMIZE 0x31u
+#define LORA_RESERVED4 0x32u
+#define LORA_INVERT_IQ 0x33u
+#define LORA_RESERVED5 0x34u
+#define LORA_RESERVED6 0x35u
+// See ERRATA 2.1 Sensitivity Optimization with a 500 kHz Bandwidth
+#define LORA_HIGH_BW_OPTIMIZE1 0x36u
+
+#define LORA_DETECTION_THRESHOLD 0x37u
+#define LORA_RESERVED7 0x38u
+#define LORA_SYNC_WORD 0x39u
+// See ERRATA 2.1 Sensitivity Optimization with a 500 kHz Bandwidth
+#define LORA_HIGH_BW_OPTIMIZE2 0x3Au
+
+#define LORA_INVERT_IQ2 0x3Bu
+#define LORA_RESERVED8 0x3Cu
+#define LORA_RESERVED9 0x3Du
+#define LORA_RESERVED10 0x3Eu
+#define LORA_RESERVED11 0x3Fu
+
 /** Structure holding RFM95W information and state. */
 struct lora
 {
@@ -39,4 +107,5 @@ uint8_t lora_get_bandwidth(struct lora *lora);
 void lora_set_coding_rate(struct lora *lora, uint8_t rate);
 uint8_t lora_get_coding_rate(struct lora *lora);
 uint8_t lora_get_register(struct lora *lora, uint8_t address);
+
 #endif//LORA_H_
--- src/lora.c
+++ src/lora.c
@@ -25,10 +25,10 @@ static void write_register(struct spi *spi, uint8_t address, uint8_t data)
 
 static void change_mode(struct spi *spi, uint8_t mode)
 {
-	uint8_t new_mode = read_register(spi, 0x01);
+	uint8_t new_mode = read_register(spi, LORA_OPMODE);
 	new_mode &= ~0x7u; // Clear mode bits
 	new_mode |= mode & 0x7u;
-	write_register(spi, 0x01, new_mode);
+	write_register(spi, LORA_OPMODE, new_mode);
 }
 
 void lora_init(struct lora *lora, uint32_t frequency)
@@ -39,30 +39,30 @@ void lora_init(struct lora *lora, uint32_t frequency)
 	lora_sleep(lora);
 
 	// Enable Lora mode
-	write_register(spi, 0x01, read_register(spi, 0x01) | 0x80);
+	write_register(spi, LORA_OPMODE, read_register(spi, 0x01) | 0x80);
 
 	if (frequency > 800000000)
 	{
-		uint8_t reg = read_register(spi, 0x01);
+		uint8_t reg = read_register(spi, LORA_OPMODE);
 		reg &= 0xF7;
-		write_register(spi, 0x01, reg);
+		write_register(spi, LORA_OPMODE, reg);
 	}
 	// FIXME what about low freq?
 
 	lora_set_frequency(lora, frequency);
 	// Set pointers for FIFOs, RX and TX
-	write_register(spi, 0x0e, 0x80);
-	write_register(spi, 0x0f, 0x00);
+	write_register(spi, LORA_FIFO_TX_BASE, 0x80);
+	write_register(spi, LORA_FIFO_RX_BASE, 0x00);
 
 	// Turn on HF LNA boost
-	write_register(spi, 0x0c, read_register(spi, 0x0c) | 0x03);
+	write_register(spi, LORA_LNA, read_register(spi, LORA_LNA) | 0x03);
 
 	// Enable AGC
-	write_register(spi, 0x26, 0x04);
+	write_register(spi, LORA_MODEM_CONFIG3, 0x04);
 
 	// Configure which pin to output of
 	// enable RFO pin, 14dB
-	write_register(spi, 0x09, 0x7E);
+	write_register(spi, LORA_PA_CONFIG, 0x7E);
 
 	lora_standby(lora);
 }
@@ -71,23 +71,23 @@ void lora_receive_packet(struct lora *lora, unsigned char buffer[], size_t buffe
 {
 	struct spi *spi = &lora->spi;
 	// If we're not in reading mode, bail
-	if ((read_register(spi, 0x1) & 0x7) != 0x05)
+	if ((read_register(spi, LORA_OPMODE) & 0x7) != 0x05)
 		return;
 
 	// FIXME check for explicit mode???
 
-	uint8_t read_irq = read_register(spi, 0x12) & 0xF0;
+	uint8_t read_irq = read_register(spi, LORA_IRQ_FLAGS) & 0xF0;
 
 	// If no CRC payload error, and RxDone
 	// Ignoring timeout and valid header IRQs? FIXME
 	if (!(read_irq & (1 << 5)) && (read_irq & (1 << 6)))
 	{
 		// FIXME should we be clearing ALL read IRQs?
-		write_register(spi, 0x12, read_irq);
+		write_register(spi, LORA_IRQ_FLAGS, read_irq);
 
-		uint8_t length = read_register(spi, 0x13);
+		uint8_t length = read_register(spi, LORA_RX_BYTES);
 		// Move current pointer to current Rx location
-		write_register(spi, 0x0D, read_register(spi, 0x10));
+		write_register(spi, LORA_FIFO_ADDR, read_register(spi, LORA_FIFO_RX_CURRENT_ADDR));
 
 		lora_standby(lora);
 
@@ -95,10 +95,10 @@ void lora_receive_packet(struct lora *lora, unsigned char buffer[], size_t buffe
 		for (uint8_t i = 0; i < length; ++i)
 		{
 			// FIXME use buffer_size!
-			buffer[i] = read_register(spi, 0x00);
+			buffer[i] = read_register(spi, LORA_FIFO);
 		}
 		// Update pointer to FIFO to beginning
-		write_register(spi, 0x0D, 0x0);
+		write_register(spi, LORA_FIFO_ADDR, 0x0);
 		lora_receive_mode(lora);
 	}
 }
@@ -111,30 +111,31 @@ void lora_send_packet(struct lora *lora, unsigned char buffer[], uint8_t buffer_
 	lora_standby(lora);
 
 	// Clear IRQ if set
-	uint8_t tx_irq = read_register(spi, 0x12) & 0x08;
-	write_register(spi, 0x12, tx_irq);
+	uint8_t tx_irq = read_register(spi, LORA_IRQ_FLAGS) & 0x08;
+	write_register(spi, LORA_IRQ_FLAGS, tx_irq);
 
 	// FIXME check for explicit mode?
 
 	// FIXME 0x80 is hardcoded TX base address
-	write_register(spi, 0x0D, 0x80);
+	write_register(spi, LORA_FIFO_ADDR, 0x80);
 	// FIXME what if buffer_size > space available in FIFO buffer?
-	write_register(spi, 0x22, buffer_size);
+	write_register(spi, LORA_PAYLOAD_LEN, buffer_size);
 
 	for (uint8_t i = 0; i < buffer_size; ++i)
 	{
-		write_register(spi, 0x00, buffer[i]);
+		write_register(spi, LORA_FIFO, buffer[i]);
 	}
 
 	lora_transmit_mode(lora);
 
-	tx_irq = read_register(spi, 0x12) & 0x08;
-	while (!(read_register(spi, 0x12 & 0x08)))
+	tx_irq = read_register(spi, LORA_IRQ_FLAGS) & 0x08;
+	while (!(read_register(spi, LORA_IRQ_FLAGS & 0x08)))
 	{
+		// FIXME is there an interrupt way to wait while going to sleep?
 		// wait
 	}
-	tx_irq = read_register(spi, 0x12) & 0x08;
-	write_register(spi, 0x12, tx_irq);
+	tx_irq = read_register(spi, LORA_IRQ_FLAGS) & 0x08;
+	write_register(spi, LORA_IRQ_FLAGS, tx_irq);
 }
 
 void lora_set_spreading_factor(struct lora *lora, uint8_t spreading_factor)
@@ -151,25 +152,25 @@ void lora_set_spreading_factor(struct lora *lora, uint8_t spreading_factor)
 	// - 0x37 must be 0x0C
 	if (spreading_factor == 6)
 	{
-		write_register(spi, 0x31, 0xC5);
-		write_register(spi, 0x37, 0x0C);
+		write_register(spi, LORA_DETECT_OPTIMIZE, 0xC5);
+		write_register(spi, LORA_DETECTION_THRESHOLD, 0x0C);
 	}
 	else
 	{
-		write_register(spi, 0x31, 0xC3);
-		write_register(spi, 0x37, 0x0A);
+		write_register(spi, LORA_DETECT_OPTIMIZE, 0xC3);
+		write_register(spi, LORA_DETECTION_THRESHOLD, 0x0A);
 	}
 
-	uint8_t config = read_register(spi, 0x1E);
+	uint8_t config = read_register(spi, LORA_MODEM_CONFIG2);
 	config &= 0x0F;
 	config |= spreading_factor << 4;
-	write_register(spi, 0x1E, config);
+	write_register(spi, LORA_MODEM_CONFIG2, config);
 }
 
 uint8_t lora_get_spreading_factor(struct lora *lora)
 {
 	struct spi *spi = &lora->spi;
-	return read_register(spi, 0x1E) >> 4;
+	return read_register(spi, LORA_MODEM_CONFIG2) >> 4;
 }
 
 uint8_t lora_rx_amount(struct lora *lora)
@@ -177,9 +178,9 @@ uint8_t lora_rx_amount(struct lora *lora)
 	struct spi *spi = &lora->spi;
 
 	// FIXME should we respond based on any of the IRQs?
-	uint8_t read_irq = read_register(spi, 0x12) & 0xF0;
+	uint8_t read_irq = read_register(spi, LORA_IRQ_FLAGS) & 0xF0;
 	if ((read_irq & 0x70) == 0x50)
-		return read_register(spi, 0x13);
+		return read_register(spi, LORA_RX_BYTES);
 	return 0;
 }
 
@@ -187,7 +188,7 @@ void lora_receive_mode(struct lora *lora)
 {
 	struct spi *spi = &lora->spi;
 	/* explicit header */
-	write_register(spi, 0x1D, read_register(spi, 0x1D) & ~0x1u);
+	write_register(spi, LORA_MODEM_CONFIG1, read_register(spi, LORA_MODEM_CONFIG1) & ~0x1u);
 	change_mode(spi, 0x05);
 }
 
@@ -195,7 +196,7 @@ void lora_transmit_mode(struct lora *lora)
 {
 	struct spi *spi = &lora->spi;
 	/* explicit header */
-	write_register(spi, 0x1D, read_register(spi, 0x1D) & ~0x1u);
+	write_register(spi, LORA_MODEM_CONFIG1, read_register(spi, LORA_MODEM_CONFIG1) & ~0x1u);
 	change_mode(spi, 0x03);
 }
 
@@ -203,7 +204,7 @@ void lora_transmit_mode(struct lora *lora)
 bool lora_transmitting(struct lora *lora)
 {
 	struct spi *spi = &lora->spi;
-	return (read_register(spi, 0x01) & 0x07) == 0x3;
+	return (read_register(spi, LORA_OPMODE) & 0x07) == 0x3;
 }
 
 void lora_sleep(struct lora *lora)
@@ -227,39 +228,39 @@ void lora_set_frequency(struct lora *lora, uint32_t frequency)
 	// fsf = f_xosc / 2^19 * frequency
 	// For this chip, f_xosc = 32MHz
 	uint32_t fsf = (1 << 19) / 32000000.0 * frequency;
-	write_register(spi, 0x06, fsf >> 16);
-	write_register(spi, 0x07, fsf >> 8);
-	write_register(spi, 0x08, fsf >> 0);
+	write_register(spi, LORA_FREQ_MSB, fsf >> 16);
+	write_register(spi, LORA_FREQ_MID, fsf >> 8);
+	write_register(spi, LORA_FREQ_LSB, fsf >> 0);
 }
 
 void lora_set_bandwidth(struct lora *lora, uint8_t bandwidth)
 {
 	struct spi *spi = &lora->spi;
-	uint8_t reg = read_register(spi, 0x1D);
+	uint8_t reg = read_register(spi, LORA_MODEM_CONFIG1);
 	reg &= 0x0F;
 	reg |= bandwidth << 4;
-	write_register(spi, 0x1D, reg);
+	write_register(spi, LORA_MODEM_CONFIG1, reg);
 }
 
 uint8_t lora_get_bandwidth(struct lora *lora)
 {
 	struct spi *spi = &lora->spi;
-	return read_register(spi, 0x1D) >> 4;
+	return read_register(spi, LORA_MODEM_CONFIG1) >> 4;
 }
 
 void lora_set_coding_rate(struct lora *lora, uint8_t rate)
 {
 	struct spi *spi = &lora->spi;
-	uint8_t reg = read_register(spi, 0x1D);
+	uint8_t reg = read_register(spi, LORA_MODEM_CONFIG1);
 	reg &= 0xF1;
 	reg |= rate << 1;
-	write_register(spi, 0x1D, reg);
+	write_register(spi, LORA_MODEM_CONFIG1, reg);
 }
 
 uint8_t lora_get_coding_rate(struct lora *lora)
 {
 	struct spi *spi = &lora->spi;
-	return (read_register(spi, 0x1D) >> 1) & 0x7;
+	return (read_register(spi, LORA_MODEM_CONFIG1) >> 1) & 0x7;
 }
 
 uint8_t lora_get_register(struct lora *lora, uint8_t address)
