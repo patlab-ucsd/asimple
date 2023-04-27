709878945cb55ab7d380e683e9de3e3b65c5fcdf
Gabriel Marcano <gabemarcano@yahoo.com>
2023-04-26 17:25:27 -0700

    Add documentation to gpio

--- include/rba_simple/gpio.h
+++ include/rba_simple/gpio.h
@@ -7,13 +7,35 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/** GPIO struct.
+ */
 struct gpio
 {
 	uint8_t pin;
 };
 
+/** Initializes the GPIO structure and given pin hardware.
+ *
+ * @param[out] gpio Pointer to the GPIO structure to initialize as a push-pull
+ *  GPIO.
+ * @param[in] Pin number of the GPIO to initialize.
+ * @param[in] Initial state of the GPIO pin.
+ */
 void gpio_init(struct gpio *gpio, uint8_t pin, bool init_state);
+
+/** Sets the GPIO pin state.
+ *
+ * @param[in,out] gpio Pointer to GPIO struct of pin to set.
+ * @param[in] state True to set to high, false to set to low.
+ */
 void gpio_set(struct gpio *gpio, bool state);
+
+/** Gets the GPIO pin state.
+ *
+ * @param[in,out] gpio Pointer to GPIO struct of pin to get.
+ *
+ * @Returns True if the pin reads high, false otherwise.
+ */
 bool gpio_read(struct gpio *gpio);
 
 #endif//GPIO_H_
