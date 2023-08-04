b645e8902dfc73135e7ac3f2b01596693e70d939
Gabriel Marcano <gabemarcano@yahoo.com>
2023-08-04 12:23:06 -0700

    cli: Add basic CLI support

---
+++ include/asimple/cli.h
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Gabriel Marcano, 2023
+
+#ifndef CLI_H_
+#define CLI_H_
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef char cli_line_buffer[32];
+
+struct ring_buffer
+{
+	bool empty;
+	size_t end;
+	size_t start;
+	cli_line_buffer data[4];
+};
+
+void ring_buffer_initialize(struct ring_buffer *buf);
+cli_line_buffer *ring_buffer_get_current(struct ring_buffer *buf);
+cli_line_buffer *ring_buffer_get(struct ring_buffer *buf, size_t index);
+void ring_buffer_advance(struct ring_buffer *buf);
+bool ring_buffer_empty(const struct ring_buffer *buf);
+size_t ring_buffer_in_use(struct ring_buffer *buf);
+
+struct cli
+{
+	bool echo;
+	struct ring_buffer history;
+};
+
+void cli_initialize(struct cli *cli);
+cli_line_buffer *cli_read_line(struct cli *cli);
+
+/** Read a line from the console.
+ *
+ * Reads a line from the console. It discards the carriage return at the end.
+ *
+ * @param[out] buf Buffer to hold the contents read from the line.
+ * @param[in] in Size of the buffer. Any additional characters past size - 1
+ *  are dropped/ignored.
+ * @param[in] echo Whether to print out incoming characters or not.
+ *
+ * @post The given buffer holds at most size - 1 characters. String in buffer
+ *  is null delimited.
+ * @returns RETURNCODE_SUCCESS on success, anything else on error.
+ */
+int read_line(char buf[], size_t size, bool echo);
+
+
+#endif//CLI_H_
---
+++ src/cli.c
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Gabriel Marcano, 2023
+
+#include "cli.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+void cli_initialize(struct cli *cli)
+{
+	ring_buffer_initialize(&cli->history);
+}
+
+int read_line(char buf[], size_t size, bool echo)
+{
+	int c;
+	size_t i = 0;
+
+	while (1)
+	{
+		c = getchar();
+		if (c == EOF)
+		{
+			buf[i] = '\0';
+			return -1;
+		}
+
+		if (echo)
+			putchar((char)c);
+		// Check for special characters
+		if (c == '\b') {
+			if (i > 0) {
+				--i;
+			}
+			if (echo)
+			{
+				putchar(' ');
+				putchar('\b');
+				fflush(stdout);
+			}
+			continue;
+		}
+		else if (c == '\r') {
+			buf[i] = '\0';
+			if (echo)
+			{
+				putchar('\n');
+				fflush(stdout);
+			}
+			break;
+		}
+		if (echo)
+			fflush(stdout);
+		if (i < size-1)
+			buf[i++] = c;
+	}
+	return 0;
+}
+
+cli_line_buffer* cli_read_line(struct cli *cli)
+{
+	cli_line_buffer *buf = ring_buffer_get(&cli->history, sizeof(cli->history)/sizeof(*buf) - 1);
+	int result = read_line((char*)buf, sizeof(*buf)/(sizeof((*buf)[0])), cli->echo);
+	if (result == 0)
+	{
+		ring_buffer_advance(&cli->history);
+		return buf;
+	}
+	return NULL;
+}
+
+void ring_buffer_initialize(struct ring_buffer *buf)
+{
+	buf->empty = true;
+	buf->end = 0;
+	buf->start = 0;
+}
+
+cli_line_buffer *ring_buffer_get(struct ring_buffer *buf, size_t index)
+{
+	const size_t ring_size = sizeof(buf->data)/sizeof(buf->data[0]);
+	return &buf->data[(buf->start - index) % ring_size];
+}
+
+void ring_buffer_advance(struct ring_buffer *buf)
+{
+	if (buf->empty)
+		buf->empty = false;
+	const size_t ring_size = sizeof(buf->data)/sizeof(buf->data[0]);
+	buf->start = (buf->start + 1) % ring_size;
+	if (buf->start == buf->end)
+		buf->end = (buf->end + 1) % ring_size;;
+}
+
+size_t ring_buffer_in_use(struct ring_buffer *buf)
+{
+	const size_t ring_size = sizeof(buf->data)/sizeof(buf->data[0]);
+	size_t result = (buf->start - buf->end + ring_size) % ring_size;
+	if (result == 0 && !buf->empty)
+		return ring_size;
+	return result;
+}
+
+bool ring_buffer_empty(const struct ring_buffer *buf)
+{
+	return buf->empty;
+}
