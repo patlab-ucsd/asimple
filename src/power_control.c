3955f7b51e2bc8008d521dff70486a74ec4957c2
Gabriel Marcano <gabemarcano@yahoo.com>
2023-08-04 12:30:19 -0700

    power_control: inverse logic
     - Artemia now assumes that the RedBoard just needs to do active high
       signaling-- this is realized in hardware because we're now using N
       MOSFETs effectively acting like inverters.

--- src/power_control.c
+++ src/power_control.c
@@ -6,12 +6,12 @@
 
 void power_control_init(struct power_control *control, uint8_t shd_pin, uint8_t active_pin)
 {
-	gpio_init(&control->shd, shd_pin, GPIO_MODE_OUTPUT, 1);
-	gpio_init(&control->active, active_pin, GPIO_MODE_OUTPUT, 0);
+	gpio_init(&control->shd, shd_pin, GPIO_MODE_OUTPUT, 0);
+	gpio_init(&control->active, active_pin, GPIO_MODE_OUTPUT, 1);
 }
 
 void power_control_shutdown(struct power_control *control)
 {
-	gpio_set(&control->active, true);
-	gpio_set(&control->shd, false);
+	gpio_set(&control->active, false);
+	gpio_set(&control->shd, true);
 }
