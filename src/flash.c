patlab-ucsd/asimple

commit b618192b387391a6226213a6e61452ae5fc34684

spi: hide uint32_t AmbiqStudioSDK details
 - Internally, the Ambiq SDK uses uint32_t for SPI buffers. This is
   obnoxious to use. Hide that detail by exposing uint8_t* instead.
 - Updated everything in the repository to use this new convention.

src/flash.c
@@ -21,18 +21,18 @@ void flash_init(struct flash *flash, struct spi_device *device)
 
 uint8_t flash_read_status_register(struct flash *flash)
 {
-	uint32_t writeBuffer = 0x05;
+	uint8_t writeBuffer = 0x05;
 	spi_device_write_continue(flash->spi, &writeBuffer, 1);
-	uint32_t readBuffer = 0;
+	uint8_t readBuffer = 0;
 	spi_device_read(flash->spi, &readBuffer, 1);
 	return (uint8_t)readBuffer;
 }
 
 void flash_wait_busy(struct flash *flash)
 {
-	uint32_t writeBuffer = 0x05;
+	uint8_t writeBuffer = 0x05;
 	spi_device_write_continue(flash->spi, &writeBuffer, 1);
-	uint32_t readBuffer = 0;
+	uint8_t readBuffer = 0;
 	do {
 		spi_device_read_continue(flash->spi, &readBuffer, 1);
 	} while (readBuffer & 0x01);
@@ -44,28 +44,23 @@ void flash_wait_busy(struct flash *flash)
 
 void flash_write_enable(struct flash *flash)
 {
-	uint32_t writeBuffer = 0x06;
+	uint8_t writeBuffer = 0x06;
 	spi_device_write(flash->spi, &writeBuffer, 1);
 }
 
 void flash_read_data(struct flash *flash, uint32_t addr, uint8_t *buffer, uint32_t size)
 {
 	// Write command as least significant bit
-	uint32_t toWrite = 0;
-	uint32_t* tmpPtr = &toWrite;
-	uint8_t* tmp = (uint8_t*) tmpPtr;
-	tmp[0] = 0x03;
-	tmp[1] = addr >> 16;
-	tmp[2] = addr >> 8;
-	tmp[3] = addr;
+	uint8_t toWrite[] = {
+		0x03,
+		addr >> 16,
+		addr >> 8,
+		addr,
+	};
+	static_assert(sizeof(toWrite) == 4, "guessed array size wrong");
 
-	spi_device_write_continue(flash->spi, &toWrite, 4);
-
-	uint32_t *data = malloc(((size+3)/4) * 4);
-	spi_device_read(flash->spi, data, size);
-	memcpy(buffer, data, size);
-	free(data);
-	data = NULL;
+	spi_device_write_continue(flash->spi, toWrite, 4);
+	spi_device_read(flash->spi, buffer, size);
 }
 
 uint8_t flash_page_program(struct flash *flash, uint32_t addr, const uint8_t *buffer, uint32_t size)
@@ -83,21 +78,16 @@ uint8_t flash_page_program(struct flash *flash, uint32_t addr, const uint8_t *bu
 	}
 
 	// Write command as least significant bit
-	uint32_t toWrite = 0;
-	uint32_t* tmpPtr = &toWrite;
-	uint8_t* tmp = (uint8_t*) tmpPtr;
-	tmp[0] = 0x02;
-	tmp[1] = addr >> 16;
-	tmp[2] = addr >> 8;
-	tmp[3] = addr;
-	spi_device_write_continue(flash->spi, &toWrite, 4);
+	uint8_t toWrite[4] = {
+		0x02,
+		addr >> 16,
+		addr >> 8,
+		addr,
+	};
+	spi_device_write_continue(flash->spi, toWrite, 4);
 
 	// Write the data
-	uint32_t *data = malloc(((size+3)/4) * 4);
-	memcpy(data, buffer, size);
-	spi_device_write(flash->spi, data, size);
-	free(data);
-	data = NULL;
+	spi_device_write(flash->spi, buffer, size);
 	return 1;
 }
 
@@ -116,22 +106,21 @@ uint8_t flash_sector_erase(struct flash *flash, uint32_t addr)
 	}
 
 	// Write command as least significant bit
-	uint32_t toWrite = 0;
-	uint32_t* tmpPtr = &toWrite;
-	uint8_t* tmp = (uint8_t*) tmpPtr;
-	tmp[0] = 0x20;
-	tmp[1] = addr >> 16;
-	tmp[2] = addr >> 8;
-	tmp[3] = addr;
-	spi_device_write(flash->spi, &toWrite, 4);
+	uint8_t toWrite[4] = {
+		0x20,
+		addr >> 16,
+		addr >> 8,
+		addr,
+	};
+	spi_device_write(flash->spi, toWrite, 4);
 	return 1;
 }
 
 uint32_t flash_read_id(struct flash *flash)
 {
-	uint32_t writeBuffer = 0x9F;
+	uint8_t writeBuffer = 0x9F;
 	spi_device_write_continue(flash->spi, &writeBuffer, 1);
-	uint32_t readBuffer = 0;
+	uint8_t readBuffer = 0;
 	spi_device_read(flash->spi, &readBuffer, 3);
 	return readBuffer;
 }

include/asimple/bmp280.h
@@ -46,7 +46,7 @@ uint8_t bmp280_read_id(struct bmp280 *bmp280);
  * @param[out] buffer Buffer that registers from the BMP280 sensor will be written into.
  * @param[in] size Number of bytes to read, starting at addr.
 */
-void bmp280_read_register(struct bmp280 *bmp280, uint32_t addr, uint32_t *buffer, uint32_t size);
+void bmp280_read_register(struct bmp280 *bmp280, uint8_t addr, uint8_t *buffer, uint32_t size);
 
 /**
  * Gets the raw temperature value from the temperature registers of the BMP280 sensor.

include/asimple/spi.h
@@ -150,7 +150,7 @@ void spi_device_set_clock(struct spi_device *device, uint32_t clock);
 void spi_device_cmd_read(
 	struct spi_device *device,
 	uint8_t command,
-	uint32_t *buffer,
+	uint8_t *buffer,
 	uint32_t size);
 
 /** Writes data (blocking) to the SPI device, sending a command byte
@@ -171,7 +171,7 @@ void spi_device_cmd_read(
 void spi_device_cmd_write(
 	struct spi_device *device,
 	uint8_t command,
-	const uint32_t *buffer,
+	const uint8_t *buffer,
 	uint32_t size);
 
 /** Reads data (blocking) from the SPI device.
@@ -185,7 +185,7 @@ void spi_device_cmd_write(
  * @param[in] size Size of the buffer.
  */
 void spi_device_read(
-	struct spi_device *device, uint32_t *buffer, uint32_t size);
+	struct spi_device *device, uint8_t *buffer, uint32_t size);
 
 /** Writes data (blocking) to the SPI device.
  *
@@ -198,7 +198,7 @@ void spi_device_read(
  * @param[in] size Size of the buffer.
  */
 void spi_device_write(
-	struct spi_device *device, const uint32_t *buffer, uint32_t size);
+	struct spi_device *device, const uint8_t *buffer, uint32_t size);
 
 /** Reads data (blocking) to the SPI device, and leave CS active (low).
  *
@@ -211,7 +211,7 @@ void spi_device_write(
  * @param[in] size Size of the buffer.
  */
 void spi_device_read_continue(
-	struct spi_device *device, uint32_t *buffer, uint32_t size);
+	struct spi_device *device, uint8_t *buffer, uint32_t size);
 
 /** Writes data (blocking) to the SPI device, and leaves CS active (low).
  *
@@ -224,7 +224,7 @@ void spi_device_read_continue(
  * @param[in] size Size of the buffer.
  */
 void spi_device_write_continue(
-	struct spi_device *device, const uint32_t *buffer, uint32_t size);
+	struct spi_device *device, const uint8_t *buffer, uint32_t size);
 
 /** Write and read data to/from the SPI device simultaneously.
  *
@@ -240,8 +240,8 @@ void spi_device_write_continue(
 void spi_device_readwrite(
 	struct spi_device *device,
 	uint32_t command,
-	uint32_t *rx_buffer,
-	const uint32_t *tx_buffer,
+	uint8_t *rx_buffer,
+	const uint8_t *tx_buffer,
 	uint32_t size);
 
 /** Toggles the SPI clock while sending 0xFF and keeping CS logical false

src/am1815.c
@@ -9,15 +9,14 @@
 
 uint8_t am1815_read_register(struct am1815 *rtc, uint8_t addr)
 {
-	uint32_t buffer;
+	uint8_t buffer;
 	spi_device_cmd_read(rtc->spi, addr, &buffer, 1);
-	return (uint8_t)buffer;
+	return buffer;
 }
 
 void am1815_write_register(struct am1815 *rtc, uint8_t addr, uint8_t data)
 {
-	uint32_t buffer = data;
-	spi_device_cmd_write(rtc->spi, 0x80 | addr, &buffer, 1);
+	spi_device_cmd_write(rtc->spi, 0x80 | addr, &data, 1);
 }
 
 static uint8_t from_bcd(uint8_t bcd)
@@ -40,10 +39,8 @@ static uint8_t to_bcd(uint8_t value)
 
 struct timeval am1815_read_time(struct am1815 *rtc)
 {
-	uint32_t buffer[2];
-	uint8_t *data = (uint8_t*)buffer;
-	spi_device_cmd_read(rtc->spi, 0x0, buffer, 8);
-	memcpy(data, buffer, 8);
+	uint8_t data[8];
+	spi_device_cmd_read(rtc->spi, 0x0, data, 8);
 
 	struct tm date = {
 		.tm_year = from_bcd(data[6]) + 100,
@@ -66,26 +63,18 @@ struct timeval am1815_read_time(struct am1815 *rtc)
 
 void am1815_read_bulk(struct am1815 *rtc, uint8_t addr, uint8_t *data, size_t size)
 {
-	uint32_t *buffer = malloc(((size + 3)/4) * 4);
-	spi_device_cmd_read(rtc->spi, addr, buffer, size);
-	memcpy(data, buffer, size);
-	free(buffer);
+	spi_device_cmd_read(rtc->spi, addr, data, size);
 }
 
 void am1815_write_bulk(struct am1815 *rtc, uint8_t addr, const uint8_t *data, size_t size)
 {
-	uint32_t *buffer = malloc(((size + 3)/4) * 4);
-	memcpy(buffer, data, size);
-	spi_device_cmd_write(rtc->spi, addr, buffer, size);
-	free(buffer);
+	spi_device_cmd_write(rtc->spi, addr, data, size);
 }
 
 struct timeval am1815_read_alarm(struct am1815 *rtc)
 {
-	uint32_t buffer[2];
-	uint8_t *data = (uint8_t*)buffer;
-	spi_device_cmd_read(rtc->spi, 0x8, buffer, 7);
-	memcpy(data, buffer, 7);
+	uint8_t data[7];
+	spi_device_cmd_read(rtc->spi, 0x8, data, 7);
 
 	struct tm date = {
         .tm_year = 0,
@@ -155,7 +144,7 @@ void am1815_init(struct am1815 *rtc, struct spi_device *device)
 
 uint8_t am1815_read_timer(struct am1815 *rtc)
 {
-	uint32_t buffer;
+	uint8_t buffer;
 	spi_device_cmd_read(rtc->spi, 0x19, &buffer, 1);
     return (uint8_t)buffer;;
 }

src/bmp280.c
@@ -19,14 +19,14 @@ void bmp280_init(struct bmp280 *sensor, struct spi_device *device)
 
 uint8_t bmp280_read_id(struct bmp280 *bmp280)
 {
-	uint32_t sensorID = 0xD0;
+	uint8_t sensorID = 0xD0;
 	spi_device_write_continue(bmp280->spi, &sensorID, 1);
-	uint32_t readBuffer = 0;
+	uint8_t readBuffer = 0;
 	spi_device_read(bmp280->spi, &readBuffer, 1);
 	return (uint8_t)readBuffer;
 }
 
-void bmp280_read_register(struct bmp280 *bmp280, uint32_t addr, uint32_t *buffer, uint32_t size)
+void bmp280_read_register(struct bmp280 *bmp280, uint8_t addr, uint8_t *buffer, uint32_t size)
 {
 	addr |= 0x80;
 	spi_device_write_continue(bmp280->spi, &addr, 1);
@@ -40,18 +40,18 @@ uint32_t bmp280_get_adc_temp(struct bmp280 *bmp280)
 	uint32_t activate = 0b00100001;
 
 	// write temp sampling to the register
-	uint32_t addr = 0xF4 & 0x7F;
-	uint32_t buffer = 0;
-	uint8_t* ptr = (uint8_t*)&buffer;
-	ptr[0] = addr;
-	ptr[1] = activate;
-	spi_device_write(bmp280->spi, &buffer, 2);
+	uint8_t addr = 0xF4 & 0x7F;
+	uint8_t buffer[2] = {
+		addr,
+		activate,
+	};
+	spi_device_write(bmp280->spi, buffer, 2);
 
-	uint32_t tempRegister = 0xFA;
+	uint8_t tempRegister = 0xFA;
 	spi_device_write_continue(bmp280->spi, &tempRegister, 1);
-	uint32_t readBuffer = 0;
-	spi_device_read(bmp280->spi, &readBuffer, 3);
-	uint8_t* ptr2 = (uint8_t*)&readBuffer;
+	uint8_t readBuffer[3] = {0};
+	spi_device_read(bmp280->spi, readBuffer, 3);
+	uint8_t* ptr2 = (uint8_t*)readBuffer;
 	uint32_t temp = ptr2[2] + (ptr2[1] << 8) + (ptr2[0] << 16);
 	temp = temp >> 4;
 	return temp;
@@ -64,38 +64,36 @@ uint32_t bmp280_get_adc_pressure(struct bmp280 *bmp280)
 	uint32_t activate = 0b00000101;
 
 	// write temp sampling to the register
-	uint32_t addr = 0xF4 & 0x7F;
-	uint32_t buffer = 0;
-	uint8_t* ptr = (uint8_t*)&buffer;
-	ptr[0] = addr;
-	ptr[1] = activate;
-	spi_device_write(bmp280->spi, &buffer, 2);
+	uint8_t addr = 0xF4 & 0x7F;
+	uint8_t buffer[2] = {
+		addr,
+		activate,
+	};
+	spi_device_write(bmp280->spi, buffer, 2);
 
-	uint32_t tempRegister = 0xF7;
+	uint8_t tempRegister = 0xF7;
 	spi_device_write_continue(bmp280->spi, &tempRegister, 1);
-	uint32_t readBuffer = 0;
-	spi_device_read(bmp280->spi, &readBuffer, 3);
+	uint8_t readBuffer[3] = {0};
+	spi_device_read(bmp280->spi, readBuffer, 3);
 	uint8_t* ptr2 = (uint8_t*)&readBuffer;
 	uint32_t temp = ptr2[2] + (ptr2[1] << 8) + (ptr2[0] << 16);
 	temp = temp >> 4;
 	return temp;
 }
 
-static uint16_t bmp280_unsigned_short_from_buffer(uint32_t* buffer)
+static uint16_t bmp280_unsigned_short_from_buffer(uint8_t* buffer)
 {
-	uint8_t* ptr = (uint8_t*)buffer;
-	uint8_t msb = ptr[1];
-	uint8_t lsb = ptr[0];
+	uint8_t msb = buffer[1];
+	uint8_t lsb = buffer[0];
 
 	uint16_t toReturn = (msb << 8) | (lsb & 0xff);
 	return toReturn;
 }
 
-static int16_t bmp280_signed_short_from_buffer(uint32_t* buffer)
+static int16_t bmp280_signed_short_from_buffer(uint8_t* buffer)
 {
-	uint8_t* ptr = (uint8_t*)buffer;
-	uint8_t msb = ptr[1];
-	uint8_t lsb = ptr[0];
+	uint8_t msb = buffer[1];
+	uint8_t lsb = buffer[0];
 
 	int16_t toReturn = (msb << 8) | (lsb & 0xff);
 	return toReturn;
@@ -103,17 +101,17 @@ static int16_t bmp280_signed_short_from_buffer(uint32_t* buffer)
 
 static uint32_t bmp280_get_t_fine(struct bmp280 *bmp280, uint32_t raw_temp)
 {
-	uint32_t dig_T1_buf;
-	bmp280_read_register(bmp280, 0x88, &dig_T1_buf, 2);		//0x88 is lsb and 0x89 is msb
-	uint16_t dig_T1 = bmp280_unsigned_short_from_buffer(&dig_T1_buf);
+	uint8_t dig_T1_buf[2];
+	bmp280_read_register(bmp280, 0x88, dig_T1_buf, 2);		//0x88 is lsb and 0x89 is msb
+	uint16_t dig_T1 = bmp280_unsigned_short_from_buffer(dig_T1_buf);
 
-	uint32_t dig_T2_buf;
-	bmp280_read_register(bmp280, 0x8A, &dig_T2_buf, 2);		//0x8A is lsb and 0x8B is msb
-	int16_t dig_T2 = bmp280_signed_short_from_buffer(&dig_T2_buf);
+	uint8_t dig_T2_buf[2];
+	bmp280_read_register(bmp280, 0x8A, dig_T2_buf, 2);		//0x8A is lsb and 0x8B is msb
+	int16_t dig_T2 = bmp280_signed_short_from_buffer(dig_T2_buf);
 
-	uint32_t dig_T3_buf;
-	bmp280_read_register(bmp280, 0x8C, &dig_T3_buf, 2);		//0x8C is lsb and 0x8D is msb
-	int16_t dig_T3 = bmp280_signed_short_from_buffer(&dig_T3_buf);
+	uint8_t dig_T3_buf[2];
+	bmp280_read_register(bmp280, 0x8C, dig_T3_buf, 2);		//0x8C is lsb and 0x8D is msb
+	int16_t dig_T3 = bmp280_signed_short_from_buffer(dig_T3_buf);
 
 	uint32_t t_fine;
 	double var1, var2;
@@ -135,41 +133,41 @@ double bmp280_compensate_P_double(struct bmp280 *bmp280, uint32_t raw_press, uin
 {
 	uint32_t t_fine = bmp280_get_t_fine(bmp280, raw_temp);
 
-	uint32_t dig_P1_buf;
-	bmp280_read_register(bmp280, 0x8E, &dig_P1_buf, 2);		//0x8E is lsb and 0x8F is msb
-	uint16_t dig_P1 = bmp280_unsigned_short_from_buffer(&dig_P1_buf);
+	uint8_t dig_P1_buf[2];
+	bmp280_read_register(bmp280, 0x8E, dig_P1_buf, 2);		//0x8E is lsb and 0x8F is msb
+	uint16_t dig_P1 = bmp280_unsigned_short_from_buffer(dig_P1_buf);
 
-	uint32_t dig_P2_buf;
-	bmp280_read_register(bmp280, 0x90, &dig_P2_buf, 2);		//0x90 is lsb and 0x91 is msb
-	int16_t dig_P2 = bmp280_signed_short_from_buffer(&dig_P2_buf);
+	uint8_t dig_P2_buf[2];
+	bmp280_read_register(bmp280, 0x90, dig_P2_buf, 2);		//0x90 is lsb and 0x91 is msb
+	int16_t dig_P2 = bmp280_signed_short_from_buffer(dig_P2_buf);
 
-	uint32_t dig_P3_buf;
-	bmp280_read_register(bmp280, 0x92, &dig_P3_buf, 2);		//0x92 is lsb and 0x93 is msb
-	int16_t dig_P3 = bmp280_signed_short_from_buffer(&dig_P3_buf);
+	uint8_t dig_P3_buf[2];
+	bmp280_read_register(bmp280, 0x92, dig_P3_buf, 2);		//0x92 is lsb and 0x93 is msb
+	int16_t dig_P3 = bmp280_signed_short_from_buffer(dig_P3_buf);
 
-	uint32_t dig_P4_buf;
-	bmp280_read_register(bmp280, 0x94, &dig_P4_buf, 2);		//0x94 is lsb and 0x95 is msb
-	int16_t dig_P4 = bmp280_signed_short_from_buffer(&dig_P4_buf);
+	uint8_t dig_P4_buf[2];
+	bmp280_read_register(bmp280, 0x94, dig_P4_buf, 2);		//0x94 is lsb and 0x95 is msb
+	int16_t dig_P4 = bmp280_signed_short_from_buffer(dig_P4_buf);
 
-	uint32_t dig_P5_buf;
-	bmp280_read_register(bmp280, 0x96, &dig_P5_buf, 2);		//0x96 is lsb and 0x97 is msb
-	int16_t dig_P5 = bmp280_signed_short_from_buffer(&dig_P5_buf);
+	uint8_t dig_P5_buf[2];
+	bmp280_read_register(bmp280, 0x96, dig_P5_buf, 2);		//0x96 is lsb and 0x97 is msb
+	int16_t dig_P5 = bmp280_signed_short_from_buffer(dig_P5_buf);
 
-	uint32_t dig_P6_buf;
-	bmp280_read_register(bmp280, 0x98, &dig_P6_buf, 2);		//0x98 is lsb and 0x99 is msb
-	int16_t dig_P6 = bmp280_signed_short_from_buffer(&dig_P6_buf);
+	uint8_t dig_P6_buf[2];
+	bmp280_read_register(bmp280, 0x98, dig_P6_buf, 2);		//0x98 is lsb and 0x99 is msb
+	int16_t dig_P6 = bmp280_signed_short_from_buffer(dig_P6_buf);
 
-	uint32_t dig_P7_buf;
-	bmp280_read_register(bmp280, 0x9A, &dig_P7_buf, 2);		//0x9A is lsb and 0x9B is msb
-	int16_t dig_P7 = bmp280_signed_short_from_buffer(&dig_P7_buf);
+	uint8_t dig_P7_buf[2];
+	bmp280_read_register(bmp280, 0x9A, dig_P7_buf, 2);		//0x9A is lsb and 0x9B is msb
+	int16_t dig_P7 = bmp280_signed_short_from_buffer(dig_P7_buf);
 
-	uint32_t dig_P8_buf;
-	bmp280_read_register(bmp280, 0x9C, &dig_P8_buf, 2);		//0x9C is lsb and 0x9D is msb
-	int16_t dig_P8 = bmp280_signed_short_from_buffer(&dig_P8_buf);
+	uint8_t dig_P8_buf[2];
+	bmp280_read_register(bmp280, 0x9C, dig_P8_buf, 2);		//0x9C is lsb and 0x9D is msb
+	int16_t dig_P8 = bmp280_signed_short_from_buffer(dig_P8_buf);
 
-	uint32_t dig_P9_buf;
-	bmp280_read_register(bmp280, 0x9E, &dig_P9_buf, 2);		//0x9E is lsb and 0x9F is msb
-	int16_t dig_P9 = bmp280_signed_short_from_buffer(&dig_P9_buf);
+	uint8_t dig_P9_buf[2];
+	bmp280_read_register(bmp280, 0x9E, dig_P9_buf, 2);		//0x9E is lsb and 0x9F is msb
+	int16_t dig_P9 = bmp280_signed_short_from_buffer(dig_P9_buf);
 
 	double var1, var2, p;
 	var1 = ((double)t_fine/2.0) - 64000.0;

src/lora.c
@@ -16,14 +16,14 @@
 
 static uint8_t read_register(struct spi_device *device, uint8_t address)
 {
-	uint32_t rx_buffer;
+	uint8_t rx_buffer;
 	spi_device_cmd_read(device, address, &rx_buffer, 1);
 	return rx_buffer;
 }
 
 static void write_register(struct spi_device *device, uint8_t address, uint8_t data)
 {
-	uint32_t tx_buffer = data;
+	uint8_t tx_buffer = data;
 	spi_device_cmd_write(device, address | 0x80, &tx_buffer, 1);
 }
 

src/spi.c
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 // Configuration structure for the IO Master.
 // FIXME I don't like why this isn't const...
@@ -311,15 +312,16 @@ bool spi_bus_enable(struct spi_bus *bus)
 }
 
 void spi_device_cmd_read(
-	struct spi_device *device, uint8_t command, uint32_t *buffer, uint32_t size)
+	struct spi_device *device, uint8_t command, uint8_t *buffer, uint32_t size)
 {
+	uint32_t *buf = malloc((size + 3)/4 * 4);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen = 1,
 		.ui32Instr = command,
 		.eDirection = AM_HAL_IOM_RX,
 		.ui32NumBytes = size,
-		.pui32RxBuffer = buffer,
+		.pui32RxBuffer = buf,
 		.bContinue = false,
 		.ui8RepeatCount = 0,
 		.ui32PauseCondition = 0,
@@ -329,19 +331,22 @@ void spi_device_cmd_read(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_blocking_transfer(device->parent->handle, &transaction);
+	memcpy(buffer, buf, size);
+	free(buf);
 }
 
 void spi_device_cmd_write(
-	struct spi_device *device, uint8_t command, const uint32_t *buffer, uint32_t size)
+	struct spi_device *device, uint8_t command, const uint8_t *buffer, uint32_t size)
 {
+	uint32_t *buf = malloc(((size + 3) / 4) * 4);
+	memcpy(buf, buffer, size);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen	= 1,
 		.ui32Instr	   = command,
 		.eDirection	  = AM_HAL_IOM_TX,
 		.ui32NumBytes	= size,
-		// FIXME I really don't like how I need to strip const here...
-		.pui32TxBuffer   = (uint32_t*)buffer,
+		.pui32TxBuffer   = buf,
 		.bContinue	   = false,
 		.ui8RepeatCount  = 0,
 		.ui32PauseCondition = 0,
@@ -351,17 +356,19 @@ void spi_device_cmd_write(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_blocking_transfer(device->parent->handle, &transaction);
+	free(buf);
 }
 
 void spi_device_read(
-	struct spi_device *device, uint32_t *buffer, uint32_t size)
+	struct spi_device *device, uint8_t *buffer, uint32_t size)
 {
+	uint32_t *buf = malloc(((size + 3) / 4) * 4);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen = 0,
 		.eDirection = AM_HAL_IOM_RX,
 		.ui32NumBytes = size,
-		.pui32RxBuffer = buffer,
+		.pui32RxBuffer = buf,
 		.bContinue = false,
 		.ui8RepeatCount = 0,
 		.ui32PauseCondition = 0,
@@ -371,18 +378,21 @@ void spi_device_read(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_blocking_transfer(device->parent->handle, &transaction);
+	memcpy(buffer, buf, size);
+	free(buf);
 }
 
 void spi_device_write(
-	struct spi_device *device, const uint32_t *buffer, uint32_t size)
+	struct spi_device *device, const uint8_t *buffer, uint32_t size)
 {
+	uint32_t *buf = malloc(((size + 3) / 4) * 4);
+	memcpy(buf, buffer, size);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen	= 0,
 		.eDirection	  = AM_HAL_IOM_TX,
 		.ui32NumBytes	= size,
-		// FIXME I really don't like how I need to strip const here...
-		.pui32TxBuffer   = (uint32_t*)buffer,
+		.pui32TxBuffer   = buf,
 		.bContinue	   = false,
 		.ui8RepeatCount  = 0,
 		.ui32PauseCondition = 0,
@@ -392,17 +402,19 @@ void spi_device_write(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_blocking_transfer(device->parent->handle, &transaction);
+	free(buf);
 }
 
 void spi_device_read_continue(
-	struct spi_device *device, uint32_t *buffer, uint32_t size)
+	struct spi_device *device, uint8_t *buffer, uint32_t size)
 {
+	uint32_t *buf = malloc(((size + 3) / 4) * 4);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen = 0,
 		.eDirection = AM_HAL_IOM_RX,
 		.ui32NumBytes = size,
-		.pui32RxBuffer = buffer,
+		.pui32RxBuffer = buf,
 		.bContinue = true,
 		.ui8RepeatCount = 0,
 		.ui32PauseCondition = 0,
@@ -412,18 +424,21 @@ void spi_device_read_continue(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_blocking_transfer(device->parent->handle, &transaction);
+	memcpy(buffer, buf, size);
+	free(buf);
 }
 
 void spi_device_write_continue(
-	struct spi_device *device, const uint32_t *buffer, uint32_t size)
+	struct spi_device *device, const uint8_t *buffer, uint32_t size)
 {
+	uint32_t *buf = malloc(((size + 3)/4) * 4);
+	memcpy(buf, buffer, size);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen	= 0,
 		.eDirection	  = AM_HAL_IOM_TX,
 		.ui32NumBytes	= size,
-		// FIXME I really don't like how I need to strip const here...
-		.pui32TxBuffer   = (uint32_t*)buffer,
+		.pui32TxBuffer   = buf,
 		.bContinue	   = true,
 		.ui8RepeatCount  = 0,
 		.ui32PauseCondition = 0,
@@ -433,15 +448,19 @@ void spi_device_write_continue(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_blocking_transfer(device->parent->handle, &transaction);
+	free(buf);
 }
 
 void spi_device_readwrite(
 	struct spi_device *device,
 	uint32_t command,
-	uint32_t *rx_buffer,
-	const uint32_t *tx_buffer,
+	uint8_t *rx_buffer,
+	const uint8_t *tx_buffer,
 	uint32_t size)
 {
+	uint32_t *bufr = malloc(((size + 3)/4) * 4);
+	uint32_t *bufw = malloc(((size + 3)/4) * 4);
+	memcpy(bufw, tx_buffer, size);
 	am_hal_iom_transfer_t transaction =
 	{
 		.ui32InstrLen	= 1,
@@ -449,8 +468,8 @@ void spi_device_readwrite(
 		.eDirection	  = AM_HAL_IOM_FULLDUPLEX,
 		.ui32NumBytes	= size,
 		// FIXME I really don't like how I need to strip const here...
-		.pui32TxBuffer   = (uint32_t*)tx_buffer,
-		.pui32RxBuffer = rx_buffer,
+		.pui32TxBuffer   = bufw,
+		.pui32RxBuffer = bufr,
 		.bContinue	   = false,
 		.ui8RepeatCount  = 0,
 		.ui32PauseCondition = 0,
@@ -460,6 +479,9 @@ void spi_device_readwrite(
 	};
 	spi_device_update_clock(device);
 	am_hal_iom_spi_blocking_fullduplex(device->parent->handle, &transaction);
+	memcpy(rx_buffer, bufr, size);
+	free(bufr);
+	free(bufw);
 }
 
 void spi_device_toggle(struct spi_device *device, uint32_t size)
@@ -469,11 +491,13 @@ void spi_device_toggle(struct spi_device *device, uint32_t size)
 	// lines, keep it high ourselves, then clock size number of bytes
 	struct gpio cs;
 	gpio_init(&cs, get_pin(device->parent->iom_module, device->chip_select), GPIO_MODE_OUTPUT, 1);
-	uint32_t data = 0xFFFFFFFFu;
+	uint8_t data[4] = {
+		0xFFu, 0xFFu, 0xFFu, 0xFFu
+	};
 	spi_device_update_clock(device);
 	for (; size > 4; size -= 4)
-		spi_device_write(device, &data, 4);
-	spi_device_write(device, &data, size);
+		spi_device_write(device, data, 4);
+	spi_device_write(device, data, size);
 	// Restore pin assignments
 	am_bsp_iom_pins_enable(device->parent->iom_module, AM_HAL_IOM_SPI_MODE);
 }

commit a51ae54db0900d12ac0b5eae69ed5fb91558d1e6

flash: fix buffer overflow when reading ID
 - Commit b618192 accidentally reduced the storage of a buffer being
   passed to a spi read command from 4 bytes to 1, but still expected
   for there to be 3 bytes. Fix that by allocating enough space for 3
   bytes.

src/flash.c
@@ -121,7 +121,8 @@ uint32_t flash_read_id(struct flash *flash)
 {
 	uint8_t writeBuffer = 0x9F;
 	spi_device_write_continue(flash->spi, &writeBuffer, 1);
-	uint8_t readBuffer = 0;
-	spi_device_read(flash->spi, &readBuffer, 3);
-	return readBuffer;
+	uint8_t readBuffer[3] = {0};
+	spi_device_read(flash->spi, readBuffer, 3);
+	uint32_t result = readBuffer[0] << 16 | readBuffer[1] << 8 | readBuffer[2];
+	return result;
 }